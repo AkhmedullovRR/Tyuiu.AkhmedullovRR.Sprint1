9abead620e5e4fe3fa594184857fd29dbdbcd57f
Равиль <fullbotrr@mail.ru>
2024-09-28 02:55:35 +0300

    Add project files.

---
+++ Tyuiu.AkhmedullovRR.Sprint1.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AkhmedullovRR.Sprint1.Task0.V19.Lib;
+
+namespace Tyuiu.AkhmeduulovRR.Sprint1.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
