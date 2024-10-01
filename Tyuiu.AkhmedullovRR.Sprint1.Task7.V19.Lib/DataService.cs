545d8b08ccadb28c3097c3f029994797146179cc
Равиль <fullbotrr@mail.ru>
2024-10-01 15:49:53 +0300

    55#

--- Tyuiu.AkhmedullovRR.Sprint1.Task7.V19.Lib/DataService.cs
+++ Tyuiu.AkhmedullovRR.Sprint1.Task7.V19.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.AkhmedullovRR.Sprint1.Task7.V19.Lib
     {
         public double Calculate(double x)
         {
-            double z = x - (7 * Math.Pow(x, 2) / Math.Pow(x, 3) + Math.Sin(x) + Math.Abs(Math.Pow(x, 4) - Math.Pow(x, 5)));
+            double z = x - 7 * Math.Pow(x, 2) / Math.Pow(x, 3) + Math.Sin(x) + Math.Abs(Math.Pow(x, 4) - Math.Pow(x, 5));
             return Math.Round(z, 3);
         }
     }
